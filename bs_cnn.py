e69ed78284a9f9256cb286863bbd5827853536f8
Aaron Lawrence <39893736+A-A-R0N@users.noreply.github.com>
2019-12-09 12:40:08 -0600

    Model updates
    Added more convolution layers with smaller filter sizes which seemed to improve the accuracy significantly. Reduced the resizing of image to improve training speed.

--- bs_cnn.py
+++ bs_cnn.py
@@ -3,39 +3,50 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense, BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam, rmsprop
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
 
 #Variable Declaration
 batch_size = 50
 training_img_nmb = 3620
 test_img_nmb = 180
-image_size = (200, 200)
+image_size = (100, 100)
 input_size = image_size + (1,)
 epochs = 500
 
 #Model Definition
 model = Sequential()
-model.add(Conv2D(32, (7, 7), input_shape=input_size))
+model.add(Conv2D(32, (3, 3), input_shape=input_size))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 
-model.add(Conv2D(64, (7, 7)))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 
-model.add(Conv2D(128, (5, 5)))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (5, 5)))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# model.add(BatchNormalization())
 # model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(256))
+model.add(Dense(128))
 model.add(Activation('relu'))
+
 # model.add(Dropout(0.25))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
@@ -49,6 +60,9 @@ model.compile(loss='binary_crossentropy',
 training_dir = os.getcwd() + "./Dataset/train"
 test_dir = os.getcwd() + "./Dataset/test"
 
+# Callbacks
+early_stop = EarlyStopping(monitor='loss', min_delta=0.001, patience=20, mode='min', verbose=1)
+checkpoint = ModelCheckpoint('model_best_weights_v5.h5', monitor='loss', verbose=1, save_best_only=True, mode='min', period=1)
 
 # Augmenting Training Data
 training_data_generator = ImageDataGenerator(
@@ -77,12 +91,15 @@ test_data_iterator = test_data_generator.flow_from_directory(
     batch_size=batch_size,
     color_mode='grayscale')
 
+
+
 model.fit_generator(
     training_data_iterator,
     steps_per_epoch=training_img_nmb // batch_size,
     epochs=epochs,
     validation_data=test_data_iterator,
-    validation_steps=test_img_nmb // batch_size
+    validation_steps=test_img_nmb // batch_size,
+    callbacks=[early_stop, checkpoint]
 )
 
 model.save('bird_squirrel.hdf5')
